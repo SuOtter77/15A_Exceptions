877775ec08030d8b824f70e6ef5ddec51798e730
AG <186358781+SuOtter77@users.noreply.github.com>
2024-12-08 21:11:20 -0500

    Added offset character functionality, with an extra check to prevent case transitions.

--- offsets.cpp
+++ offsets.cpp
@@ -1,2 +1,15 @@
 // implement character offsets function
 #include "offsets.h"
+#include <ctype.h>
+
+char character(char start, int offset) {
+	char target = start + offset;
+	const int distance = offset;
+	if (offset > 31 || offset < -32)
+		throw "outOfBoundary"; // prevent case transition
+	if (!isalpha(start))
+		throw "invalidCharacterException";
+	if (!isalpha(target))
+		throw "invalidRangeException";
+	return target;
+}
